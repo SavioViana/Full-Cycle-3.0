114a263a73bf27d18fea895ce67c0591d9742442
Savio Viana <savio.viana13@gmail.com>
2024-08-14 22:46:15 -0300

    feat: ddd-patterns - add test

--- ddd-patterns/src/entity/customer.spec.ts
+++ ddd-patterns/src/entity/customer.spec.ts
@@ -1,6 +1,56 @@
+import Address from "./address"
+import Customer from "./customer"
+
 describe("Customer unit tests", () => {
 
-    it('test', () => {
-        expect(1).toBe(1)
+    it('should throw error when id is empty', () => {
+
+        expect(() => {
+            let customer = new Customer("", "Jonh")
+        }).toThrow("Id is required")
     })
+
+    it('should throw error when name is empty', () => {
+
+        expect(() => {
+            let customer = new Customer("123", "")
+        }).toThrow("Name is required")
+    })
+
+    it('should change name', () => {
+
+        const customer = new Customer("123", "John")
+
+        customer.changeName("Jane")
+
+        expect(customer.name).toBe("Jane")
+
+    })
+
+    it('should activate customer', () => {
+
+        const customer = new Customer("1", "Customer 1")
+        const address = new Address("Rua 01", 2, "46430-000", "Guanambi")
+        customer.Address = address
+
+        customer.activate()
+
+        expect(customer.isActive()).toBe(true)
+    })
+
+    it('should desactivate customer', () => {
+
+        const customer = new Customer("1", "Customer 1")
+        customer.desactivate()
+        expect(customer.isActive()).toBe(false)
+    })
+
+    it('should thown error when address is undefinded', () => {
+        
+        expect(() => {
+            const customer = new Customer("1", "Customer 1")
+            customer.activate()
+        }).toThrow("Address is mandatory to activate a customer")
+    })
+
 })
--- ddd-patterns/src/entity/customer.ts
+++ ddd-patterns/src/entity/customer.ts
@@ -1,15 +1,24 @@
 import Address from "./address";
 
 export default class Customer {
-    _id: string;
-    _name: string;
-    _address!: Address
-    _active: boolean = false
+    private _id: string;
+    private _name: string;
+    private _address!: Address
+    private _active: boolean = false
 
     constructor(id: string, name: string) {
         this._id = id;
         this._name = name
         this.validate()
+    }
+
+    
+    public get name() : string {
+        return this._name
+    }
+    
+    public isActive() : boolean {
+        return this._active
     }
 
     validate() {
